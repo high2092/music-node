d7e9625586a2f43dfdef786f81d2f89c4dad7029
hhyuw <high2092@gmail.com>
2023-05-26 17:16:53 +0900

    fix: 앵커 드랍 이벤트를 컴포넌트 레벨로 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,45 +30,32 @@ function Home() {
     localStorage.setItem(LOCAL_STORAGE_KEY, code);
   }, [musics, musicNodes]);
 
-  useEffect(() => {
-    const preventDefault = (e: DragEvent) => {
-      e.preventDefault();
-    };
+  const handleAnchorDrop = async (e: React.DragEvent) => {
+    const url = e.dataTransfer.getData('text/plain') || e.dataTransfer.getData('text/uri-list');
+    const regex = /^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([^&]+)/;
 
-    const handleAnchorDrag = async (e: DragEvent) => {
-      const url = e.dataTransfer.getData('text/plain') || e.dataTransfer.getData('text/uri-list');
-      const regex = /^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([^&]+)/;
+    const match = url.match(regex);
 
-      const match = url.match(regex);
+    if (!match) return;
+    const videoId = match[1];
 
-      if (!match) return;
-      const videoId = match[1];
+    const response = await fetch(`/api/video-title?videoId=${videoId}`);
+    if (!response.ok) {
+      console.error(response.statusText);
+      return;
+    }
 
-      const response = await fetch(`/api/video-title?videoId=${videoId}`);
-      if (!response.ok) {
-        console.error(response.statusText);
-        return;
-      }
+    const { title } = await response.json();
+    const music = musicService.createMusic(title, videoId);
+    const position = reactFlowInstance.project({ x: e.clientX, y: e.clientY });
+    const musicNode = musicNodeService.createMusicNode(title, videoId, position);
 
-      const { title } = await response.json();
-      const music = musicService.createMusic(title, videoId);
-      const position = reactFlowInstance.project({ x: e.clientX, y: e.clientY });
-      const musicNode = musicNodeService.createMusicNode(title, videoId, position);
-
-      dispatch(addMusic(music));
-      dispatch(createMusicNode(musicNode));
-    };
-
-    window.addEventListener('dragover', preventDefault);
-    window.addEventListener('drop', handleAnchorDrag);
-    return () => {
-      window.removeEventListener('dragover', preventDefault);
-      window.removeEventListener('drop', handleAnchorDrag);
-    };
-  }, []);
+    dispatch(addMusic(music));
+    dispatch(createMusicNode(musicNode));
+  };
 
   return (
-    <S.Home>
+    <S.Home onDrop={handleAnchorDrop}>
       <S.NodeManagerSection>
         <NodeManager />
       </S.NodeManagerSection>
