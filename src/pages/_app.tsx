555a1681aceaee17234cd36a6e0d017b1676af0c
hhyuw <high2092@gmail.com>
2023-05-28 23:01:33 +0900

    a11y: 문서 title 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,14 @@ import '../../public/css/reset.css';
 import { Provider } from 'react-redux';
 import { store } from '../features/store';
 import { ModalContainer } from '../components/modal/ModalContainer';
+import Head from 'next/head';
 
 function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Music Node</title>
+      </Head>
       <Component {...pageProps} />
       <ModalContainer />
     </Provider>
