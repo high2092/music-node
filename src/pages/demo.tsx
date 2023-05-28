8a3552d5348977234062572081c5b780a07c896f
hhyuw <high2092@gmail.com>
2023-05-28 16:49:44 +0900

    fix: 로컬 스토리지에 값이 있을 경우 데모 페이지 스킵

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch } from '../features/store';
 import { decodeV1 } from '../utils/encoding';
 import { load } from '../features/mainSlice';
 import { useRouter } from 'next/router';
+import { LOCAL_STORAGE_KEY } from '../constants/interface';
 
 function DemoPage() {
   const dispatch = useAppDispatch();
@@ -10,12 +11,15 @@ function DemoPage() {
   const router = useRouter();
 
   useEffect(() => {
-    fetch('/data/demo.mnode', { method: 'GET' })
-      .then((response) => response.text())
-      .then((code) => {
-        const { musics, musicNodes } = decodeV1(code);
-        dispatch(load({ musics, musicNodes }));
-      });
+    const code = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (!code) {
+      fetch('/data/demo.mnode', { method: 'GET' })
+        .then((response) => response.text())
+        .then((code) => {
+          const { musics, musicNodes } = decodeV1(code);
+          dispatch(load({ musics, musicNodes }));
+        });
+    }
 
     router.push('/');
   }, []);
