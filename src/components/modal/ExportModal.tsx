25743fccfe01186dd0b76a4dba12ac5be0184ca3
hhyuw <high2092@gmail.com>
2023-05-28 23:45:20 +0900

    feat: 파일 저장 시 자동으로 모달 닫히도록 변경

--- src/components/modal/ExportModal.tsx
+++ src/components/modal/ExportModal.tsx
@@ -2,20 +2,23 @@ import { FieldValues, useForm } from 'react-hook-form';
 import { ModalStyle } from '../../styles/common/ModalStyle';
 import { PreparedModalProps } from '../../types/modal';
 import { CenteredModal } from './Modal';
-import { useAppSelector } from '../../features/store';
+import { useAppDispatch, useAppSelector } from '../../features/store';
 import { encodeV1 } from '../../utils/encoding';
 import { downloadFile } from '../../utils/file';
+import { closeModal } from '../../features/modalSlice';
 
 export function ExportModal({ zIndex }: PreparedModalProps) {
   return <CenteredModal content={<Content />} zIndex={zIndex} />;
 }
 
 function Content() {
+  const dispatch = useAppDispatch();
   const { musics, musicNodes } = useAppSelector((state) => state.main);
   const { register, handleSubmit } = useForm();
 
   const handleSave = ({ filename }: FieldValues) => {
     downloadFile(filename, encodeV1(musics, musicNodes));
+    dispatch(closeModal());
   };
 
   return (
