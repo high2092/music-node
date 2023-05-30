bc10bc7f88e8cbe1fe0cf363beab059e85596e2d
hhyuw <high2092@gmail.com>
2023-05-30 20:23:55 +0900

    design: 노드 스타일링

--- src/constants/style.ts
+++ src/constants/style.ts
@@ -1,3 +1,5 @@
+import { CSSProperties } from 'react';
+
 export const ICON_SIZE = '1rem';
 export const PLAY_ICON_SIZE = '1.5rem';
 export const CD_ICON_SIZE = '1.5rem';
@@ -8,3 +10,15 @@ export const TOP_BAR_HEIGHT = `calc(${PLAY_ICON_SIZE} + 2rem)`;
 export const DEFAULT_NODE_COLOR = 'white';
 export const UI_MINI_CRITERIA_MAX_WIDTH = '1000px';
 export const UI_MINIMIZE_CRITERIA_MAX_WIDTH = '600px';
+export const NODE_STYLE: CSSProperties = {
+  minHeight: '2.5rem',
+  minWidth: '6rem',
+  maxWidth: 'max-content',
+
+  border: '1px solid black',
+  borderRadius: '1rem',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
