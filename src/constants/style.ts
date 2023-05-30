97b1083d49e8ef8c9d222e02eda17a44efdae759
hhyuw <high2092@gmail.com>
2023-05-30 19:50:44 +0900

    design: UI 최소화 기준 너비 변경

--- src/constants/style.ts
+++ src/constants/style.ts
@@ -7,4 +7,4 @@ export const MODAL_DEFAULT_BORDER_RADIUS = '20px';
 export const TOP_BAR_HEIGHT = `calc(${PLAY_ICON_SIZE} + 2rem)`;
 export const DEFAULT_NODE_COLOR = 'white';
 export const UI_MINI_CRITERIA_MAX_WIDTH = '1000px';
-export const UI_MINIMIZE_CRITERIA_MAX_WIDTH = '700px';
+export const UI_MINIMIZE_CRITERIA_MAX_WIDTH = '600px';
