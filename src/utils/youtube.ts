31eed382de741fe858ce5421fb702274d58115de
hhyuw <high2092@gmail.com>
2023-06-05 20:47:54 +0900

    feat: URL 지원 범위 확장

--- src/utils/youtube.ts
+++ src/utils/youtube.ts
@@ -1,5 +1,5 @@
 export function extractVideoId(url: string) {
-  const regexList = [/^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([^&]+)/, /^https:\/\/www\.google\.com\/search\?gs_ssp=.+?&vld=.+?,vid:([^,]+)/];
+  const regexList = [/^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([A-Za-z0-9_-]+)/, /^https:\/\/www\.google\.com\/search\?.*?vid:([A-Za-z0-9_-]+)$/, /^https:\/\/youtu\.be\/([A-Za-z0-9_-]+)/];
   for (const regex of regexList) {
     const match = url.match(regex);
     if (match) return match[1];
