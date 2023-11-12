a6fc76b587e4858a40e60e1a6c1bb82c424709a0
hhyuw <high2092@gmail.com>
2023-11-13 05:08:20 +0900

    feat: 공지사항 오류 수정

--- src/components/modals/NoticeModal/NoticeModal.tsx
+++ src/components/modals/NoticeModal/NoticeModal.tsx
@@ -12,7 +12,7 @@ function Content() {
       <div className={title}>공지사항</div>
       <div className={notice}>{'플레이리스트가 서버에 저장되도록 바뀌었어요. (2023. 11. 12)'}</div>
       <div className={subNotice}>{'카카오 계정으로 로그인할 수 있어요.'}</div>
-      <div className={subNotice}>{'좌측 상단 <로컬 상태를 서버에 반영하기>를 클릭해 기존 플레이리스트를 불러올 수 있어요.ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ(계정당 최초 1회만 가능하며 로컬 상태는 사라집니다.)'}</div>
+      <div className={subNotice}>{'좌측 상단 <로컬 상태를 서버에 반영하기>를 클릭해 기존 플레이리스트를 불러올 수 있어요. (계정당 최초 1회 가능)'}</div>
     </div>
   );
 }
